eda71d4423df9139eed3a853e2de09390d9ae94d
Michale F <ferreiramichael661@gmail.com>
2026-06-23 21:57:03 -0300

    feat: atualiza e adicionar resposta com filtro de entrada do servidor

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,32 @@
 import http from 'node:http';
 
+const users = [
+	{
+		name: 'Cristiano Ronaldo',
+		status: true,
+	},
+	{
+		name: 'Lionel Messi',
+		status: true,
+	},
+	{
+		name: 'Neymar Júnior',
+		status: false,
+	},
+];
+
 http
-	.createServer(() => {
-		console.log('Knock on the door!');
+	.createServer((request, response) => {
+		if (request.url === '/users' && request.method === 'GET') {
+			response.writeHead(200, {
+				'content-type': 'application/json',
+			});
+			response.end(JSON.stringify(users));
+			return;
+		}
+		response.writeHead(404, {
+			'content-type': 'text/plain',
+		});
+		response.end('Not found!');
 	})
-	.listen(3000);
+	.listen(Number(process.env.PORT));
