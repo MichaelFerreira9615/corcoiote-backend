98956b624d6617599b5cbc1507600118ddb31d19
Michale F <ferreiramichael661@gmail.com>
2026-07-07 22:07:53 -0300

    feat: corringindo linha de codigo

--- src/mocks/customer.mock.ts
+++ src/mocks/customer.mock.ts
@@ -4,19 +4,19 @@ export const customers: Customer[] = [
 	{
 		id: 1,
 		name: 'Cristiano Ronaldo',
-		email: 'cris@email.com',
+		email: 'cr7@email.com',
 		status: true,
 	},
 	{
 		id: 2,
 		name: 'Lionel Messi',
-		email: 'lionel@email.com',
+		email: 'leo@email.com',
 		status: true,
 	},
 	{
 		id: 3,
 		name: 'Neymar Júnior',
-		email: 'neymar@email.com',
+		email: 'ney@email.com',
 		status: false,
 	},
 ];
